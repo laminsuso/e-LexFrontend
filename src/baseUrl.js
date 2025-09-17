282553bb0ee558ce2a046bdc9934fd905b28d03f
Lamin Suso <susolamin@gmail.com>
2025-09-17 09:40:21 -0500

    backend

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,6 +1,10 @@
 // export const BASE_URL="http://localhost:6000"
 
 // export const BASE_URL="https://e-lex-backend.vercel.app"
-export const BASE_URL="e-lex-backend-eight.vercel.app"
+// export const BASE_URL="e-lex-backend-eight.vercel.app"
+// src/baseUrl.js
+export const BASE_URL =
+  process.env.REACT_APP_API_URL || "https://e-lex-backend-eight.vercel.app";
+
 
     
